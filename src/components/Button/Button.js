38c18c45a1d54ad81b26c217d4153291f251b868
LuluNull <lucylemonyay@gmail.com>
2022-05-28 13:20:40 -0300

    buttons onClick updated

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import './Button.css'
 
 function Button(props) {
     return (
-      <button className='button' id='hello'>
+      <button className='button' id='hello' onClick={ () => console.log ('Clicked!')}>
         {props.buttonName}
       </button>
     );
